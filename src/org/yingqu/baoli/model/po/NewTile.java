e71f4e8a060aada5aa3ffec2ebfd3f563b6bc076
houlynn <houlynn@gzinterest.com>
2014-10-25 09:44:54 +0800

    10yue25

---
+++ src/org/yingqu/baoli/model/po/NewTile.java
@@ -0,0 +1,29 @@
+package org.yingqu.baoli.model.po;
+
+public class NewTile {
+	
+	private String img;
+	private String title;
+	private String newId;
+	
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getNewId() {
+		return newId;
+	}
+	public void setNewId(String newId) {
+		this.newId = newId;
+	}
+	
+
+}
